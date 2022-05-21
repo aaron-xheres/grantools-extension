a1dc17eeb971dbca1f58f468a66c0fee01d03998
Aaron Tan <aarontan.jr@gmail.com>
2022-05-22 04:40:55 +0800

    Changed Local Storage to Chrome Storage | Added Debugger

--- src/background.ts
+++ src/background.ts
@@ -1,164 +1,166 @@
-import * as CONST from '@/const';
-
-// Local Storage
-import localStorage from '@/localStorage';
-
-if (Object.keys(localStorage.getData(CONST.STORES.ACTIONS)).length === 0) {
-  localStorage.setData(
-      CONST.STORES.ACTIONS,
-      CONST.STORE_ACTIONS.autoRefresh,
-      false,
-  );
-  localStorage.setData(
-      CONST.STORES.ACTIONS,
-      CONST.STORE_ACTIONS.repeatStage,
-      false,
-  );
-}
-if (Object.keys(localStorage.getData(CONST.STORES.DATA)).length === 0) {
-  localStorage.setData(
-      CONST.STORES.DATA,
-      CONST.STORE_DATA.currentPageType,
-      CONST.PAGE_TYPE.NULL,
-  );
-  localStorage.setData(CONST.STORES.DATA, CONST.STORE_DATA.refreshAttack, true);
-  localStorage.setData(
-      CONST.STORES.DATA,
-      CONST.STORE_DATA.refreshSummon,
-      false,
-  );
-  localStorage.setData(
-      CONST.STORES.DATA,
-      CONST.STORE_DATA.lastStartedStage,
-      '',
-  );
-  localStorage.setData(
-      CONST.STORES.DATA,
-      CONST.STORE_DATA.repeatStageCounter,
-      0,
-  );
-}
-
-const actions = localStorage.getData(CONST.STORES.ACTIONS);
-console.log('%c [ACTIONS STORE]', 'color: #bada55', actions);
-
-const data = localStorage.getData(CONST.STORES.DATA);
-console.log('%c [DATA STORE]', 'color: #bada55', data);
-
-// States
 import {reactive} from 'vue';
 
+import * as CONST from '@/const';
+import localStorage from '@/localStorage';
+
 export const actionStore = reactive({
-  autoRefresh: actions.autoRefresh,
-  repeatStage: actions.repeatStage,
+  autoRefresh: false,
+  repeatStage: false,
 });
 
 export const dataStore = reactive({
-  currentPageType: data.currentPageType,
-  refreshAttack: data.refreshAttack,
-  refreshSummon: data.refreshSummon,
-  lastStartedStage: data.lastStartedStage,
-  repeatStageCounter: data.repeatStageCounter,
+  currentPageType: CONST.PAGE_TYPE.NULL,
+  refreshAttack: true,
+  refreshSummon: false,
+  lastStartedStage: '',
+  repeatStageCounter: 0,
 });
 
-// Functions
-let [tab]: any = [];
+const initStores = async () => {
+  const storage = await localStorage.getAllData();
+  if (Object.keys(storage).length === 0) {
+    await localStorage.setData(CONST.STORE_ACTIONS.autoRefresh, false);
+    await localStorage.setData(CONST.STORE_ACTIONS.repeatStage, false);
+    await localStorage.setData(
+        CONST.STORE_DATA.currentPageType,
+        CONST.PAGE_TYPE.NULL,
+    );
+    await localStorage.setData(CONST.STORE_DATA.refreshAttack, true);
+    await localStorage.setData(CONST.STORE_DATA.refreshSummon, false);
+    await localStorage.setData(CONST.STORE_DATA.lastStartedStage, '');
+    await localStorage.setData(CONST.STORE_DATA.repeatStageCounter, 0);
+  }
 
-const initTabs = async () => {
-  tab = await chrome.tabs.query({active: true, currentWindow: true});
-  console.log(tab);
+  actionStore.autoRefresh = storage.autoRefresh;
+  actionStore.repeatStage = storage.repeatStage;
+
+  dataStore.currentPageType = storage.currentPageType;
+  dataStore.refreshAttack = storage.refreshAttack;
+  dataStore.refreshSummon = storage.refreshSummon;
+  dataStore.lastStartedStage = storage.lastStartedStage;
+  dataStore.repeatStageCounter = storage.repeatStageCounter;
+
+  console.log('[CHROME STORAGE]', storage);
 };
 
-initTabs();
+let [tab]: any = [];
+const initTabs = async () => {
+  tab = await chrome.tabs.query({active: true, currentWindow: true});
+  console.log('[TAB DATA]', tab);
+};
 
-const setStore = (
-    store: string,
-    dataKey: string,
-    dataValue: any,
-    log?: boolean,
-) => {
-  if (store === CONST.STORES.ACTIONS) {
-    switch (dataKey) {
-      case CONST.STORE_ACTIONS.autoRefresh:
-        actionStore.autoRefresh = dataValue;
-        localStorage.setData(
-            CONST.STORES.ACTIONS,
-            CONST.STORE_ACTIONS.autoRefresh,
-            dataValue,
-            log,
-        );
-        break;
-      case CONST.STORE_ACTIONS.repeatStage:
-        actionStore.repeatStage = dataValue;
-        localStorage.setData(
-            CONST.STORES.ACTIONS,
-            CONST.STORE_ACTIONS.repeatStage,
-            dataValue,
-            log,
-        );
-        break;
-    }
-  }
-  else if (store === CONST.STORES.DATA) {
-    switch (dataKey) {
-      case CONST.STORE_DATA.currentPageType:
-        dataStore.currentPageType = dataValue;
-        localStorage.setData(
-            CONST.STORES.DATA,
-            CONST.STORE_DATA.currentPageType,
-            dataValue,
-            log,
-        );
-        break;
-      case CONST.STORE_DATA.refreshAttack:
-        dataStore.refreshAttack = dataValue;
-        localStorage.setData(
-            CONST.STORES.DATA,
-            CONST.STORE_DATA.refreshAttack,
-            dataValue,
-            log,
-        );
-        break;
-      case CONST.STORE_DATA.refreshSummon:
-        dataStore.refreshSummon = dataValue;
-        localStorage.setData(
-            CONST.STORES.DATA,
-            CONST.STORE_DATA.refreshSummon,
-            dataValue,
-            log,
-        );
-        break;
-      case CONST.STORE_DATA.lastStartedStage:
-        dataStore.lastStartedStage = dataValue;
-        localStorage.setData(
-            CONST.STORES.DATA,
-            CONST.STORE_DATA.lastStartedStage,
-            dataValue,
-            log,
-        );
-        break;
-      case CONST.STORE_DATA.repeatStageCounter:
-        dataStore.repeatStageCounter = dataValue;
-        localStorage.setData(
-            CONST.STORES.DATA,
-            CONST.STORE_DATA.repeatStageCounter,
-            dataValue,
-            log,
-        );
-        break;
-    }
+const initDebugger = async () => {
+  const debuggee = {tabId: tab[0].id};
+  const filter = [
+    '_result.json',
+    'http://game.granbluefantasy.jp/result/data/',
+    'http://game.granbluefantasy.jp/resultmulti/data/',
+  ];
+
+  chrome.debugger.attach(debuggee, '1.3', (): void => {
+    chrome.debugger.sendCommand(debuggee, 'Network.enable');
+    console.log('[DEBUGGER] Attach Successful');
+  });
+
+  chrome.debugger.onDetach.addListener((source, reason): void => {
+    console.warn('[DEBUGGER] Detach due to', reason);
+  });
+
+  chrome.debugger.onEvent.addListener(
+      (source, method: string, params: any): void => {
+        if (method === 'Network.responseReceived') {
+          if (filter.some((url) => params.response.url.includes(url))) {
+            try {
+              helloFromExtension();
+              chrome.debugger.sendCommand(
+                  debuggee,
+                  'Network.getResponseBody',
+                  {requestId: params.requestId},
+                  (result) => {
+                    const dataType: string = params.response.url.includes('.json?') ?
+                  params.response.url.split('/').pop().split('.').shift() :
+                  'reward';
+                    console.log('[NETWORK DATA]', dataType);
+                  },
+              );
+            }
+            catch (err) {}
+          }
+        }
+      },
+  );
+};
+
+(async () => {
+  await initStores();
+  await initTabs();
+  await initDebugger();
+})();
+
+const setStore = async (dataKey: string, dataValue: any, log?: boolean) => {
+  switch (dataKey) {
+    case CONST.STORE_ACTIONS.autoRefresh:
+      actionStore.autoRefresh = dataValue;
+      await localStorage.setData(
+          CONST.STORE_ACTIONS.autoRefresh,
+          dataValue,
+          log,
+      );
+      break;
+    case CONST.STORE_ACTIONS.repeatStage:
+      actionStore.repeatStage = dataValue;
+      await localStorage.setData(
+          CONST.STORE_ACTIONS.repeatStage,
+          dataValue,
+          log,
+      );
+      break;
+    case CONST.STORE_DATA.currentPageType:
+      dataStore.currentPageType = dataValue;
+      await localStorage.setData(
+          CONST.STORE_DATA.currentPageType,
+          dataValue,
+          log,
+      );
+      break;
+    case CONST.STORE_DATA.refreshAttack:
+      dataStore.refreshAttack = dataValue;
+      await localStorage.setData(
+          CONST.STORE_DATA.refreshAttack,
+          dataValue,
+          log,
+      );
+      break;
+    case CONST.STORE_DATA.refreshSummon:
+      dataStore.refreshSummon = dataValue;
+      await localStorage.setData(
+          CONST.STORE_DATA.refreshSummon,
+          dataValue,
+          log,
+      );
+      break;
+    case CONST.STORE_DATA.lastStartedStage:
+      dataStore.lastStartedStage = dataValue;
+      await localStorage.setData(
+          CONST.STORE_DATA.lastStartedStage,
+          dataValue,
+          log,
+      );
+      break;
+    case CONST.STORE_DATA.repeatStageCounter:
+      dataStore.repeatStageCounter = dataValue;
+      await localStorage.setData(
+          CONST.STORE_DATA.repeatStageCounter,
+          dataValue,
+          log,
+      );
+      break;
   }
 };
 
 export const helloFromExtension = async () => {
+  setStore(CONST.STORE_ACTIONS.autoRefresh, !actionStore.autoRefresh, true);
   setStore(
-      CONST.STORES.ACTIONS,
-      CONST.STORE_ACTIONS.autoRefresh,
-      !actionStore.autoRefresh,
-      true,
-  );
-  setStore(
-      CONST.STORES.DATA,
       CONST.STORE_DATA.repeatStageCounter,
       dataStore.repeatStageCounter + 1,
       true,
@@ -172,16 +174,12 @@ export const helloFromExtension = async () => {
 };
 
 export const resetStores = async () => {
-  setStore(CONST.STORES.ACTIONS, CONST.STORE_ACTIONS.autoRefresh, false);
-  setStore(CONST.STORES.ACTIONS, CONST.STORE_ACTIONS.repeatStage, false);
-  setStore(
-      CONST.STORES.DATA,
-      CONST.STORE_DATA.currentPageType,
-      CONST.PAGE_TYPE.NULL,
-  );
-  setStore(CONST.STORES.DATA, CONST.STORE_DATA.refreshAttack, true);
-  setStore(CONST.STORES.DATA, CONST.STORE_DATA.refreshSummon, false);
-  setStore(CONST.STORES.DATA, CONST.STORE_DATA.lastStartedStage, '');
-  setStore(CONST.STORES.DATA, CONST.STORE_DATA.repeatStageCounter, 0);
+  setStore(CONST.STORE_ACTIONS.autoRefresh, false);
+  setStore(CONST.STORE_ACTIONS.repeatStage, false);
+  setStore(CONST.STORE_DATA.currentPageType, CONST.PAGE_TYPE.NULL);
+  setStore(CONST.STORE_DATA.refreshAttack, true);
+  setStore(CONST.STORE_DATA.refreshSummon, false);
+  setStore(CONST.STORE_DATA.lastStartedStage, '');
+  setStore(CONST.STORE_DATA.repeatStageCounter, 0);
   console.warn('[STORES] RESET STORE');
 };
--- src/localStorage.ts
+++ src/localStorage.ts
@@ -1,53 +1,51 @@
 /**
- * Local Storage Management
+ * Chrome Storage Management
  */
 export class localStorage {
   /**
    * Get Data from Storage of key
-   * @param {string} storageKey
+   * @param {string} dataKey
    * @return {Record<string, any>}
    */
-  static getData(storageKey: string): Record<string, any> {
-    const dataStr = window.localStorage.getItem(storageKey);
-    if (dataStr) {
-      const data = JSON.parse(dataStr as string);
+  static async getData(dataKey: string): Promise<Record<string, any>> {
+    const data = chrome.storage.sync.get(dataKey);
+    if (data) {
       return data;
     }
     else {
-      console.error('[LOCAL STORAGE] Storage Key does not exist:', storageKey);
+      console.error('[LOCAL STORAGE] Storage Key does not exist:', dataKey);
       return {};
     }
   }
 
   /**
-   * Set Specfici Data to Storage of key
-   * @param {string} storageKey
+   * Set Data to Storage
    * @param {string} dataKey
    * @param {any} dataValue
    * @param {boolean} log
    */
-  static setData(
-      storageKey: string,
+  static async setData(
       dataKey: string,
       dataValue: any,
       log?: boolean,
-  ): void {
-    const data = this.getData(storageKey);
-
-    data[dataKey] = dataValue;
-
-    const newDataStr = JSON.stringify(data);
-    window.localStorage.setItem(storageKey, newDataStr);
+  ): Promise<void> {
+    chrome.storage.sync.set({[dataKey]: dataValue});
     if (log) {
       console.log(
           '[LOCAL STORAGE] Storage:',
-          storageKey,
           ' |  Data Set:',
           dataKey,
           ': ',
           dataValue,
       );
     }
+  }
+
+  /**
+   * Get all Storage Data
+   */
+  static async getAllData() {
+    return chrome.storage.sync.get(null);
   }
 }
 
